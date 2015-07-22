17eb5f4ea501426a0073e3daedbf551cc81a189d
Grapker <zerex93@gmail.com>
2015-07-22 23:34:03 +0200

    nwd app

---
+++ cs/spojApps/spojApps/NWD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spojApps
+{
+    class NWD
+    {
+        public void run()
+        {
+            Console.WriteLine("Type number of tests");
+            String input;
+            input = Console.ReadLine();
+            int range;
+            try
+            {
+                range = Int32.Parse(input);
+                String[] digits = new String[range];
+                for (int i = 0; i < range; i++)
+                {
+                    input = Console.ReadLine();
+                    digits[i] = input;
+                }
+
+                foreach (String iterator in digits)
+                {
+                }
+
+            }
+        }
+    }
+}
